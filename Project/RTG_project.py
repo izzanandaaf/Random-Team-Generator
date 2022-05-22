4a498d1fa6f1a37177466b0888bae8ceb891cfa8
Izzananda <izzanadimasfaza15@student.uns.ac.id>
2022-05-22 14:02:29 +0700

    Team Project Prak Prokom Kelompok 11

---
+++ Project/RTG_project.py
@@ -0,0 +1,44 @@
+import random
+
+nama = [
+    "Izzan",
+    "Mirza",
+    "Erwin",
+    "Najwa",
+    "Kanaya"
+    ]
+
+tim = int(input("Jumlah Kelompok: "))
+member = len(nama)//tim
+lebih = len(nama) % tim
+
+keluar = []
+
+def tampilkanNama():
+    nomor = random.randrange(len(nama)) #randomnya antara 0 sampai 27
+    if len(keluar) == 0:
+        print("\t\t", nama[nomor])
+        keluar.append(nomor)
+    else:
+        for i in range(len(keluar)):
+            if nomor == keluar[i]:
+                cek = False
+                break
+            else:
+                cek = True
+        
+        if cek:
+            print("\t\t", nama[nomor])
+            keluar.append(nomor)
+        else:
+            tampilkanNama()
+        
+
+for i in range(tim): #menampilkan kelompok
+    print("Kelompok", i+1)
+    if i < lebih:
+        for j in range(member+1): #menampilkan nama
+            tampilkanNama()
+    else:
+        for j in range(member): #menampilkan nama
+            tampilkanNama()
